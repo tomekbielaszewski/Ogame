68ab617ef81380d507fe72402add480712a278bb
Grizwold <tomekbielaszewski@gmail.com>
2016-05-13 22:20:40 +0200

    Initial application structure and dependencies

---
+++ src/main/java/org/grizz/OgameCloneApplication.java
@@ -0,0 +1,12 @@
+package org.grizz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OgameCloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OgameCloneApplication.class, args);
+	}
+}
